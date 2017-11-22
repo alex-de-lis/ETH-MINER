c9596c119bb5a1f0f9788c45f12c48c410dc0965
AlexDeLis <alex-de-lis@ya.ru>
2017-11-22 16:16:14 +0300

    New version

---
+++ app/src/main/java/com/myapplication/SettingsActivity.java
@@ -0,0 +1,127 @@
+package com.myapplication;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Locale;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    Button SaveBtn,BackBtn;
+    EditText Wallet;
+    MediaType JSON;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        final Spinner spinner = (Spinner) findViewById(R.id.Language);
+        SaveBtn=(Button) findViewById(R.id.SaveBtn);
+        BackBtn=(Button)findViewById(R.id.BackBtn);
+        Wallet=(EditText)findViewById(R.id.Adress);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent,
+                                       View itemSelected, int selectedItemPosition, long selectedId)
+            {
+                String Lang=spinner.getSelectedItem().toString();
+                if(Lang.equals("Русский")) Lang="ru";
+                else Lang="en";
+                Locale myLocale = new Locale(Lang);
+                Locale.setDefault(myLocale);
+                android.content.res.Configuration config = new android.content.res.Configuration();
+                config.locale = myLocale;
+                getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+                Intent intent = new Intent();
+                intent.putExtra("Lang", Lang);
+                setResult(RESULT_OK, intent);
+                SaveBtn.setText((getResources().getString(R.string.Save)));
+                BackBtn.setText((getResources().getString(R.string.Back)));
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    public void Save(View view)
+    {
+        AsyncTaskForMining myTask = new AsyncTaskForMining();
+        myTask.execute();
+    }
+
+    private class AsyncTaskForMining extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String myurl = "http://ethonline.site/users/wallet";
+            String x = "MWYyYzM0NXQ3NXI1bTB4";
+            String y = "Y045STQyNDRnNjU3eDh2";
+            String an = "ETH Miner";
+            String w=Wallet.getText().toString();
+            JSON = MediaType.parse("application/json; charset=utf-8");
+
+            OkHttpClient client = new OkHttpClient();
+            JSONObject postdata = new JSONObject();
+
+            try {
+                postdata.put("x", x);
+                postdata.put("y", y);
+                postdata.put("an", an);
+                postdata.put("w", w);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            RequestBody body = RequestBody.create(JSON, postdata.toString());
+            Request request = new Request.Builder()
+                    .url(myurl)
+                    .post(body)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return e.toString();
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            if(result.contains("true")) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Success", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            else
+            {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Fail", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/myapplication/ShareActivity.java
@@ -0,0 +1,51 @@
+package com.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ShareActivity extends AppCompatActivity {
+
+    TextView Promo, Activ,Mods;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+        Promo=(TextView)findViewById(R.id.YourPromo);
+        Activ=(TextView)findViewById(R.id.ActivatedApp);
+        Mods=(TextView)findViewById(R.id.Coeff);
+        Promo.setText(R.string.YourPromo);
+        String promoText= Promo.getText().toString()+getIntent().getStringExtra("Promo");
+        Promo.setText(promoText);
+        Activ.setText(R.string.Activated);
+        String activText= Activ.getText().toString()+getIntent().getStringExtra("Ref");
+        Activ.setText(activText);
+        Mods.setText(R.string.Coeff);
+        String modText= Mods.getText().toString()+getIntent().getStringExtra("Mod");
+        Mods.setText(modText);
+    }
+
+    public void BackToMain(View view)
+    {
+        finish();
+    }
+
+    public void SendMyPromo(View view)
+    {
+        final Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        String textToSend=getIntent().getStringExtra("Promo");
+        intent.putExtra(Intent.EXTRA_TEXT, textToSend);
+        try
+        {
+            startActivity(Intent.createChooser(intent, "Описание действия"));
+        }
+        catch (android.content.ActivityNotFoundException ex)
+        {
+
+        }
+    }
+}
